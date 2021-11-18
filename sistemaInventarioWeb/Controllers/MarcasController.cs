a5e3223fd674da09df17f3206dc020a62c513b41
Enso <Gurix@DESKTOP-06R4K9S>
2021-11-18 09:27:42 -0300

    Agregar archivos de proyecto.

---
+++ sistemaInventarioWeb/Controllers/MarcasController.cs
@@ -0,0 +1,73 @@
+﻿using sistemaInventarioWeb.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace sistemaInventarioWeb.Controllers
+{
+    public class MarcasController : Controller
+    {
+        private ventasdbEntities db = new ventasdbEntities();
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Marca marca)
+        {
+            db.Marca.Add(marca);
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult Index()
+        {
+            var listaMarcas = db.Marca.ToList();
+            return View(listaMarcas);
+        }
+        //variable int? significa que puede ser nulo 
+        public ActionResult Edit(int? id)
+        {
+            if(id != null)
+            {
+                var marca = db.Marca.Find(id);
+                if(marca != null)
+                    return View(marca);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Edit(Marca marca)
+        {
+            //editamos con los datos enviados
+            db.Entry(marca).State = EntityState.Modified;
+            //guardo los cambios en la db
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var marca = db.Marca.Find(id);
+                if (marca != null)
+                    return View(marca);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var marca = db.Marca.Find(id);
+            if(marca != null)
+            {
+                //quitar el registro de la marca
+                db.Marca.Remove(marca);
+                //guardar los cambios
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ sistemaInventarioWeb/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("sistemaInventarioWeb")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sistemaInventarioWeb")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en True en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como ID de typelib si este proyecto se expone a COM
+[assembly: Guid("8af16555-1d31-437f-8d7c-40af2f2b475c")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
